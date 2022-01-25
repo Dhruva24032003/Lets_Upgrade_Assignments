07e64dc9b85eaa6ed28d95622fa06b8c6d7086d6
Dhruva24032003 <94905683+Dhruva24032003@users.noreply.github.com>
2022-01-25 15:41:18 +0530

    Day1 Assignment

---
+++ AOC.js
@@ -0,0 +1,10 @@
+//calculate the area of the circle using variables and operators
+
+let pi = 3.1416;
+let radius = 4;
+
+let areaofcircle;
+
+areaofcircle = pi*radius*radius;
+
+ console.log("areaofcircle = "+areaofcircle);
